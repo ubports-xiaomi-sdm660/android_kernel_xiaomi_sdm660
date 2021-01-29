2ebafbd180d28d70ee2e711d28e3ac3d30d1a2b3
Sultan Alsawaf <sultan@kerneltoast.com>
2021-01-28 19:18:13 -0800

    sched/fair: Don't remove important task migration logic from PELT
    This task migration logic is guarded by a WALT #ifdef even though it has
    nothing specific to do with WALT. The result is that, with PELT, boosted
    tasks can be migrated to the little cluster, causing visible stutters.
    
    Move the WALT #ifdef so PELT can benefit from this logic as well.
    
    Thanks to Zachariah Kennedy <zkennedy87@gmail.com> and Danny Lin
    <danny@kdrag0n.dev> for discovering this issue and creating this fix.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8969,13 +8969,13 @@ int can_migrate_task(struct task_struct *p, struct lb_env *env)
 		return 0;
 #endif
 
+	/* Don't detach task if it is under active migration */
+	if (env->src_rq->push_task == p)
+		return 0;
+
 	/* Don't detach task if it doesn't fit on the destination */
 	if (env->flags & LBF_IGNORE_BIG_TASKS &&
 		!task_fits_max(p, env->dst_cpu))
-		return 0;
-
-	/* Don't detach task if it is under active migration */
-	if (env->src_rq->push_task == p)
 		return 0;
 
 	if (task_running(env->src_rq, p)) {
