76203a583cb2641a45ae3c9d69e80f64f949b956
DennySPb <dennyspb@gmail.com>
2021-05-25 11:19:17 +0300

    power_supply/qcom: Revert TIME_TO_FULL_NOW reporting
    When quick charging kernel reports wrong values to userspace. Better remove this property at all.
    
    This commit reverts 1e4fbcb18664ee390f369b9777a36fa3b63fb54d
    
    and partially reverts 8046f269c077bb33248fd7c06d9f2d84df847533
    
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- drivers/power/supply/qcom/qpnp-smb2.c
+++ drivers/power/supply/qcom/qpnp-smb2.c
@@ -1155,7 +1155,6 @@ static enum power_supply_property smb2_batt_props[] = {
 #endif
 	POWER_SUPPLY_PROP_CHARGE_FULL,
 	POWER_SUPPLY_PROP_CHARGE_FULL_DESIGN,
-	POWER_SUPPLY_PROP_TIME_TO_FULL_NOW,
 	POWER_SUPPLY_PROP_CYCLE_COUNT,
 	POWER_SUPPLY_PROP_FCC_STEPPER_ENABLE,
 };
@@ -1288,7 +1287,6 @@ static int smb2_batt_get_prop(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_CYCLE_COUNT:
 	case POWER_SUPPLY_PROP_VOLTAGE_NOW:
 	case POWER_SUPPLY_PROP_TEMP:
-	case POWER_SUPPLY_PROP_TIME_TO_FULL_NOW:
 		rc = smblib_get_prop_from_bms(chg, psp, val);
 		break;
 	case POWER_SUPPLY_PROP_CURRENT_NOW:
