9235cb386e4fe03da4753596f69aaac9cf8885fe
Alfred Neumayer <dev.beidl@gmail.com>
2021-12-14 11:22:23 +0100

    binder: Configurable global PID lookups
    This reshakes commit d2587ce3968c6fdde3cf09d785bb957c4b75572f
    and implements a kernel parameter to explicitly allow disabling
    the behavior change to look up caller PIDs in the global namespace.

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -159,6 +159,9 @@ static int binder_set_stop_on_user_error(const char *val,
 module_param_call(stop_on_user_error, binder_set_stop_on_user_error,
 	param_get_int, &binder_stop_on_user_error, 0644);
 
+static bool binder_global_pid_lookups = true;
+module_param_named(global_pid_lookups, binder_global_pid_lookups, bool, S_IRUGO);
+
 #define binder_debug(mask, x...) \
 	do { \
 		if (binder_debug_mask & mask) \
@@ -4318,6 +4321,9 @@ retry:
 
 			tr.sender_pid = task_tgid_nr_ns(sender,
 							task_active_pid_ns(current));
+
+			if (binder_global_pid_lookups && tr.sender_pid == 0)
+				tr.sender_pid = task_tgid_nr(sender);
 		} else {
 			tr.sender_pid = 0;
 		}
